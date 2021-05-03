1c3d02ef6479532af42e43e7a11c96a54e6fb3d1
hardaowe000 <47753759+hardaowe000@users.noreply.github.com>
2021-05-03 14:37:03 -0500

    Update trollcoin.py

--- trollcoin.py
+++ trollcoin.py
@@ -259,169 +259,172 @@ async def on_message(message):
   global channels
   global pdata
   # print(message.channel.type)
-  if str(message.channel.type) == "private" and pm.startswith(prefix+"mine"):
-    if str(message.author.id) in pdata.keys():
-      pdata[str(message.author.id)] = pdata[str(message.author.id)] + .1
-    else:
-      message.channel.send(f"<@{message.author.id}>, you don't have an account! Type tc!create to make an account.")
+  if message.author.bot == True:
+    pass
+  else:
+    if str(message.channel.type) == "private" and pm.startswith(prefix+"mine"):
+      if str(message.author.id) in pdata.keys():
+        pdata[str(message.author.id)] = pdata[str(message.author.id)] + .1
+      else:
+        message.channel.send(f"<@{message.author.id}>, you don't have an account! Type tc!create to make an account.")
+  
+    if pm.startswith(prefix+"bind") and str(message.channel.type) != "private" and (message.author == message.channel.guild.owner or message.author.guild_permissions.administrator == True):
+      if message.channel.id in channels:
+        await message.channel.send("Channel already binded.")
+      else:
+        channels.append(message.channel.id)
 
-  if pm.startswith(prefix+"bind") and str(message.channel.type) != "private" and (message.author == message.channel.guild.owner or message.author.guild_permissions.administrator == True):
-    if message.channel.id in channels:
-      await message.channel.send("Channel already binded.")
-    else:
-      channels.append(message.channel.id)
+        # print(channels)
+        await message.channel.send("Binded successfully.")
 
-      # print(channels)
-      await message.channel.send("Binded successfully.")
-
-  if int(message.channel.id) in channels or str(message.channel.type) == "private":
-    if message.author == client.user:
-      return
-    elif pm.startswith(prefix+"help"):
+   if int(message.channel.id) in channels or str(message.channel.type) == "private":
+      if message.author == client.user:
+        return
+      elif pm.startswith(prefix+"help"):
       
-      await message.channel.send(embed=helpEmbed())
+        await message.channel.send(embed=helpEmbed())
       
-    elif pm.startswith(prefix+"unbind") and str(message.channel.type) != "private" and (message.author == message.channel.guild.owner or message.author.guild_permissions.administrator == True):
+      elif pm.startswith(prefix+"unbind") and str(message.channel.type) != "private" and (message.author == message.channel.guild.owner or message.author.guild_permissions.administrator == True):
 
-      channels.pop(channels.index(message.channel.id))
+        channels.pop(channels.index(message.channel.id))
 
-      await message.channel.send("Channel unbinded successfully.")
+        await message.channel.send("Channel unbinded successfully.")
 
-    elif pm.startswith(prefix+"create"):
+      elif pm.startswith(prefix+"create"):
 
       # input(data)
 
-      if str(message.author.id) in pdata.keys():
-        await message.channel.send(f"Whoops, <@{message.author.id}>, you have an account already!")
-      else:
-        pdata.setdefault(str(message.author.id), 50)
-        # pdata = json.dumps(pdata)
-        # data = open(os.path.join(sys.path[0], "data.txt"),'w')
-        # data.write(pdata)
-        await message.channel.send(f"Thank you <@{message.author.id}>, your account has been created!")
-
-    elif pm.startswith(prefix+"bal"):
-
-      if message.mentions == []:
         if str(message.author.id) in pdata.keys():
-          await message.channel.send(f"<@{message.author.id}>, you have {round(pdata[str(message.author.id)],2):,}~~TC~~!")
+          await message.channel.send(f"Whoops, <@{message.author.id}>, you have an account already!")
         else:
-          await message.channel.send(f"<@{message.author.id}>, you don't have an account! Type tc!create to make an account.")
-      elif message.mentions != [] and str(message.mentions[0].id) in pdata.keys():
-        await message.channel.send(f"{message.mentions[0].name} has {round(pdata[str(message.mentions[0].id)],2):,}~~TC~~.")
-      else:
-        await message.channel.send(f"{message.mentions[0].name} doesn't have an account. You know what would be epic? Telling them to make one!")
+          pdata.setdefault(str(message.author.id), 50)
+          # pdata = json.dumps(pdata)
+          # data = open(os.path.join(sys.path[0], "data.txt"),'w')
+          # data.write(pdata)
+          await message.channel.send(f"Thank you <@{message.author.id}>, your account has been created!")
 
-    elif pm.startswith(prefix+"send"):
-      try:
+     elif pm.startswith(prefix+"bal"):
+
         if message.mentions == []:
-          await message.channel.send(f"Oh yikes, <@{message.author.id}>, you need to mention (ping) someone for this command, mate!")
-        elif str(message.author.id) not in pdata.keys():
-          await message.channel.send(f"<@{message.author.id}>, you don't have an account! Type {prefix}create to make an account.")
-        elif str(message.mentions[0].id) not in pdata.keys():
-          await message.channel.send(f"{message.mentions[0].name} doesn't have an account. You know what would be epic? Telling them to make one!")
-        else:
-          # # print(pm.split())
-          transfer_amount = round(float(pm.split()[-1]),2)
-          # # print(transfer_amount)
-          if pdata[str(message.author.id)] < transfer_amount+1:
-            await message.channel.send(f"<@{message.author.id}>, you don't have enough TrollCoin for this transfer! :(")
+          if str(message.author.id) in pdata.keys():
+            await message.channel.send(f"<@{message.author.id}>, you have {round(pdata[str(message.author.id)],2):,}~~TC~~!")
           else:
-            pdata[str(message.author.id)] = pdata[str(message.author.id)] - transfer_amount
-            pdata[str(message.mentions[0].id)] = pdata[str(message.mentions[0].id)] + transfer_amount
-            await message.channel.send(f"<@{message.author.id}>, your transfer of {transfer_amount} to {message.mentions[0].name} was successful!")
+            await message.channel.send(f"<@{message.author.id}>, you don't have an account! Type tc!create to make an account.")
+        elif message.mentions != [] and str(message.mentions[0].id) in pdata.keys():
+          await message.channel.send(f"{message.mentions[0].name} has {round(pdata[str(message.mentions[0].id)],2):,}~~TC~~.")
+        else:
+          await message.channel.send(f"{message.mentions[0].name} doesn't have an account. You know what would be epic? Telling them to make one!")
+
+      elif pm.startswith(prefix+"send"):
+        try:
+          if message.mentions == []:
+            await message.channel.send(f"Oh yikes, <@{message.author.id}>, you need to mention (ping) someone for this command, mate!")
+          elif str(message.author.id) not in pdata.keys():
+            await message.channel.send(f"<@{message.author.id}>, you don't have an account! Type {prefix}create to make an account.")
+          elif str(message.mentions[0].id) not in pdata.keys():
+            await message.channel.send(f"{message.mentions[0].name} doesn't have an account. You know what would be epic? Telling them to make one!")
+          else:
+            # # print(pm.split())
+            transfer_amount = round(float(pm.split()[-1]),2)
+            # # print(transfer_amount)
+            if pdata[str(message.author.id)] < transfer_amount+1:
+              await message.channel.send(f"<@{message.author.id}>, you don't have enough TrollCoin for this transfer! :(")
+            else:
+              pdata[str(message.author.id)] = pdata[str(message.author.id)] - transfer_amount
+              pdata[str(message.mentions[0].id)] = pdata[str(message.mentions[0].id)] + transfer_amount
+              await message.channel.send(f"<@{message.author.id}>, your transfer of {transfer_amount} to {message.mentions[0].name} was successful!")
     
-      except:
-        await message.channel.send("Yikes, something went wrong. Perhaps it's because you formatted this command incorrectly. It should be {prefix}send <usermention> <intamount>. Ignore the <>'s. Good luck!'")
-    elif pm.startswith(prefix+"changelog"):
-      await message.channel.send(embed=changeLog())
-    elif pm.startswith(prefix+"ahelp"):
-      # dm = await message.author.create_dm()
-      x = 0
-      ahelp_dm = await message.channel.send(embed = ahelp(x))
-      await ahelp_dm.add_reaction("➡️")
-      def ahelpcheck(reaction,user):
-        return user == message.author and str(reaction.emoji) == "➡️"
-      while True:
-        try:
-          # print(x)
-          await client.wait_for('reaction_add',timeout=30,check=ahelpcheck)
-        except asyncio.TimeoutError:
-          break
-        else:
-          await ahelp_dm.remove_reaction(member=message.author,emoji="➡️")
-          x = 0 if x > 0 else x + 1
-          await ahelp_dm.edit(embed=ahelp(x))
-    elif pm.startswith(prefix+"ascend"):
-      if pdata[str(message.author.id)] < 1001:
-        await message.channel.send(f"<@{message.author.id}>, you don't have enough TrollCoin to ascend! :(")
-      else:
-        sure_message = await message.channel.send("You are about to spend 1k~~TC~~ on an invite to TrollCoin heaven. Are you sure if you want to do this?")
-        await sure_message.add_reaction("✔️")
-        # await sure_message.add_reaction("❌")
+        except:
+         await message.channel.send("Yikes, something went wrong. Perhaps it's because you formatted this command incorrectly. It should be {prefix}send <usermention> <intamount>. Ignore the <>'s. Good luck!'")
+     elif pm.startswith(prefix+"changelog"):
+       await message.channel.send(embed=changeLog())
+     elif pm.startswith(prefix+"ahelp"):
+       # dm = await message.author.create_dm()
+       x = 0
+       ahelp_dm = await message.channel.send(embed = ahelp(x))
+        await ahelp_dm.add_reaction("➡️")
+        def ahelpcheck(reaction,user):
+         return user == message.author and str(reaction.emoji) == "➡️"
+       while True:
+         try:
+           # print(x)
+           await client.wait_for('reaction_add',timeout=30,check=ahelpcheck)
+         except asyncio.TimeoutError:
+           break
+         else:
+           await ahelp_dm.remove_reaction(member=message.author,emoji="➡️")
+           x = 0 if x > 0 else x + 1
+           await ahelp_dm.edit(embed=ahelp(x))
+     elif pm.startswith(prefix+"ascend"):
+       if pdata[str(message.author.id)] < 1001:
+         await message.channel.send(f"<@{message.author.id}>, you don't have enough TrollCoin to ascend! :(")
+       else:
+         sure_message = await message.channel.send("You are about to spend 1k~~TC~~ on an invite to TrollCoin heaven. Are you sure if you want to do this?")
+         await sure_message.add_reaction("✔️")
+          # await sure_message.add_reaction("❌")
+          def check(reaction,user):
+           return user == message.author and str(reaction.emoji) == "✔️"
+         try:
+           await client.wait_for("reaction_add",timeout=30,check=check)
+         except asyncio.TimeoutError:
+           await sure_message.reply("Ok, maybe next time :)")
+         else:
+           pdata[str(message.author.id)] = pdata[str(message.author.id)] - 1000
+           temp_inv = await client.get_channel(836267451574124564).create_invite(unique=True,max_uses=1)
+           dm = await message.author.create_dm()
+           await dm.send(content=temp_inv)
+      elif pm.startswith(prefix+"bail"):
+        x = await message.channel.send("You're about to reset your account and gain back 25~~TC~~. Are you sure you want to do this?")
+        await x.add_reaction("✔️")
         def check(reaction,user):
-          return user == message.author and str(reaction.emoji) == "✔️"
-        try:
-          await client.wait_for("reaction_add",timeout=30,check=check)
-        except asyncio.TimeoutError:
-          await sure_message.reply("Ok, maybe next time :)")
+         return user == message.author and str(reaction.emoji) == "✔️"
+       try:
+         await client.wait_for("reaction_add",timeout=120,check=check)
+       except:pass
+       else: pdata[str(message.author.id)] = 25; await message.channel.send(f"<@{message.author.id}>, you've been bailed out.")
+
+     elif pm.startswith(prefix+"scratch"):
+        if str(message.author.id) not in pdata.keys():
+          await message.channel.send(f"<@{message.author.id}>, you don't have an account! Type {prefix}create to make an account.")
+        elif pdata[str(message.author.id)] < 7:
+          await message.channel.send(f"<@{message.author.id}>, it seems like you don't have enough money for this scratch ticket!")
         else:
-          pdata[str(message.author.id)] = pdata[str(message.author.id)] - 1000
-          temp_inv = await client.get_channel(836267451574124564).create_invite(unique=True,max_uses=1)
-          dm = await message.author.create_dm()
-          await dm.send(content=temp_inv)
-    elif pm.startswith(prefix+"bail"):
-      x = await message.channel.send("You're about to reset your account and gain back 25~~TC~~. Are you sure you want to do this?")
-      await x.add_reaction("✔️")
-      def check(reaction,user):
-        return user == message.author and str(reaction.emoji) == "✔️"
-      try:
-        await client.wait_for("reaction_add",timeout=120,check=check)
-      except:pass
-      else: pdata[str(message.author.id)] = 25; await message.channel.send(f"<@{message.author.id}>, you've been bailed out.")
+          winner_ticket = [str(random.randint(0,9)) for f in range(5)]
+          your_ticket =   [str(random.randint(0,9)) for f in range(5)]
+          ticket_mod = random.randint(0, 3)
+          if ticket_mod == 0:
+            mod_loc=None
+            ticket_mod=None
+            pass
+         elif ticket_mod == 1:
+            mod_loc = random.randint(0,len(your_ticket)-2)
+            for m in range(2):your_ticket[mod_loc+m] = winner_ticket[mod_loc+m]
+         elif ticket_mod == 2:
+            mod_loc = random.randint(0,len(your_ticket)-3)
+            for m in range(3):your_ticket[mod_loc+m] = winner_ticket[mod_loc+m]
+          elif ticket_mod == 3:
+            mod_loc = random.randint(0,len(your_ticket)-4)
+            for m in range(4):your_ticket[mod_loc+m] = winner_ticket[mod_loc+m]
 
-    elif pm.startswith(prefix+"scratch"):
-      if str(message.author.id) not in pdata.keys():
-        await message.channel.send(f"<@{message.author.id}>, you don't have an account! Type {prefix}create to make an account.")
-      elif pdata[str(message.author.id)] < 7:
-        await message.channel.send(f"<@{message.author.id}>, it seems like you don't have enough money for this scratch ticket!")
-      else:
-        winner_ticket = [str(random.randint(0,9)) for f in range(5)]
-        your_ticket =   [str(random.randint(0,9)) for f in range(5)]
-        ticket_mod = random.randint(0, 3)
-        if ticket_mod == 0:
-          mod_loc=None
-          ticket_mod=None
-          pass
-        elif ticket_mod == 1:
-          mod_loc = random.randint(0,len(your_ticket)-2)
-          for m in range(2):your_ticket[mod_loc+m] = winner_ticket[mod_loc+m]
-        elif ticket_mod == 2:
-          mod_loc = random.randint(0,len(your_ticket)-3)
-          for m in range(3):your_ticket[mod_loc+m] = winner_ticket[mod_loc+m]
-        elif ticket_mod == 3:
-          mod_loc = random.randint(0,len(your_ticket)-4)
-          for m in range(4):your_ticket[mod_loc+m] = winner_ticket[mod_loc+m]
+         # print(ticket_mod)
+         # print(mod_loc)
 
-        # print(ticket_mod)
-        # print(mod_loc)
+         # # print(winner_combo)
+         pdata[str(message.author.id)] = pdata[str(message.author.id)] -5
 
-        # # print(winner_combo)
-        pdata[str(message.author.id)] = pdata[str(message.author.id)] -5
+         if winner_ticket == your_ticket:
+           pdata[str(message.author.id)] = pdata[str(message.author.id)] + 50000
 
-        if winner_ticket == your_ticket:
-          pdata[str(message.author.id)] = pdata[str(message.author.id)] + 50000
+         await message.channel.send(embed = scratchTicket(winner_ticket,your_ticket))
 
-        await message.channel.send(embed = scratchTicket(winner_ticket,your_ticket))
-
-    else: pass
+     else: pass
   
-  if str(message.channel.type) != "private" and pm.startswith(prefix+"help") and int(message.channel.id) not in channels:
-    await message.channel.send(content=f"Type in {prefix}bind to bind this channel. Otherwise I cannot interract with most commands.\n\nNote than you **must have admin permisssions to do this.**")
-  if pm.startswith("le") and pm in ["".join([x,"o"*(len(pm)-len(x))]) for x in lesgo]:
-    await message.add_reaction("👉")
-    await message.add_reaction("👶")
-    await message.add_reaction("👈")
+   if str(message.channel.type) != "private" and pm.startswith(prefix+"help") and int(message.channel.id) not in channels:
+     await message.channel.send(content=f"Type in {prefix}bind to bind this channel. Otherwise I cannot interract with most commands.\n\nNote than you **must have admin permisssions to do this.**")
+   if pm.startswith("le") and pm in ["".join([x,"o"*(len(pm)-len(x))]) for x in lesgo]:
+     await message.add_reaction("👉")
+     await message.add_reaction("👶")
+     await message.add_reaction("👈")
 
 @client.event
 async def on_member_join(member):
