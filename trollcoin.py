81de94fd8398977cbb660f173c59f799d3fdd418
hardaowe000 <47753759+hardaowe000@users.noreply.github.com>
2021-05-19 09:57:11 -0500

    Update trollcoin.py

--- trollcoin.py
+++ trollcoin.py
@@ -391,7 +391,9 @@ async def on_message(message):
       elif pm.startswith(prefix+"stock"):
         stock = str(pm.split()[-1])
         try:
-          await message.channel.send(content=f"{stock.upper()} stock price: {BeautifulSoup(rq.get("https://www.google.com/search?q="+stock.replace(" ","+")+"+stock+price",headers={"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"}).text,"html.parser").find(jsname="vWLAgc").text}")
+          html_page = BeautifulSoup(rq.get("https://www.google.com/search?q="+stock.rstrip().replace(" ","+")+"+stock+price",headers={"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"}).text,"html.parser")
+          await message.channel.send(content=f"{stock.upper()} stock price: {html_page.find(jsname='vWLAgc').text}")
+
         except:
           await message.channel.send(content="An Error Occured :(")
   
